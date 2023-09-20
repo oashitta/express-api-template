b2f41e8014d3a36799ddba658dcd56d88160f610
oashitta <omotunde.shitta@gmail.com>
2023-09-20 09:05:06 -0400

    environment variables

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 // const express = require("express");
 import express from 'express';
+import { testEnvironmentVariable } from '../config';
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Express API template' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
 
 // module.exports = indexRouter;
 export default indexRouter;
